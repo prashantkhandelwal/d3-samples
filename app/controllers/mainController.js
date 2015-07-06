62e92a404311d131e9dc3f8cac2bb4b0af85b4c3
Prashant Khandelwal <prashant@midnightprogrammer.net>
2015-07-06 10:12:41 +0530

    Initial commit

---
+++ app/controllers/mainController.js
@@ -0,0 +1,30 @@
+'use strict'
+
+angular.module('d3csvjsonApp')
+.controller('mainCtrl', ['$scope', '$parse', function($scope, $parse) {
+	$scope.csv = {
+		content: null,
+		header: true,
+		separator: ',',
+		result: null
+	};
+
+ var _lastGoodResult = '';
+    $scope.toPrettyJSON = function (json, tabWidth) {
+			var objStr = JSON.stringify(json);
+			var obj = null;
+			try {
+				obj = $parse(objStr)({});
+			} catch(e){
+				return _lastGoodResult;
+			}
+
+			var result = JSON.stringify(obj, null, Number(tabWidth));
+			_lastGoodResult = result;
+
+			return result;
+    };
+
+
+
+}]);
